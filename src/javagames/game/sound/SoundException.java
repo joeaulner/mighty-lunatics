5b866fed48351fe31d154703cd4bcc277dcb88a3
nnskalka <nnskalka@gmail.com>
2017-05-02 04:30:45 -0500

    Removed all warnings

--- src/javagames/game/sound/SoundException.java
+++ src/javagames/game/sound/SoundException.java
@@ -1,7 +1,8 @@
 package javagames.game.sound;
 
 public class SoundException extends RuntimeException {
-	
+	private static final long serialVersionUID = -2824856894087843480L;
+
 	public SoundException(String message) {
 		super(message);
 	}
